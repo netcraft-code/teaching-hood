08645e71bfdfb693b6e49dff42d3f78dc9cb5847
Abhishek <akathuria289gmail.com>
2026-02-16 12:33:18 +0530

    added import functionality

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -4,9 +4,12 @@ namespace App\Console\Commands;
 
 use App\Models\City;
 use App\Models\GradeLevel;
+use App\Models\JobPost;
 use App\Models\State;
 use App\Models\Subject;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ImportData extends Command
@@ -43,7 +46,7 @@ class ImportData extends Command
 
             // City
             if (!empty($data[2])) {
-                City::updateOrCreate(
+                $city = City::updateOrCreate(
                     ['name' => trim($data[2])],
                     ['name' => trim($data[2])]
                 );
@@ -51,26 +54,102 @@ class ImportData extends Command
 
             // State
             if (!empty($data[3])) {
-                State::updateOrCreate(
+                $state = State::updateOrCreate(
                     ['name' => trim($data[3])],
                     ['name' => trim($data[3])]
                 );
             }
 
             // Subject
+            $subject = NULL;
             if (!empty($data[7])) {
-                Subject::updateOrCreate(
+                $subject = Subject::updateOrCreate(
                     ['name' => trim($data[7])],
                     ['name' => trim($data[7])]
                 );
             }
 
             // Grade Level
+            $grade = NULl;
             if (!empty($data[8])) {
-                GradeLevel::updateOrCreate(
+                $grade = GradeLevel::updateOrCreate(
                     ['name' => trim($data[8])],
                     ['name' => trim($data[8])]
                 );
+            }
+
+            $schoolName = $data[1];
+            $board = $data[5];
+            $minSalary = null;
+            $maxSalary = null;
+
+            if (!empty($data[9])) {
+                $salaryParts = explode('-', $data[9]);
+
+                if (isset($salaryParts[0])) {
+                    $minSalary = (int) str_replace(',', '', trim($salaryParts[0]));
+                }
+
+                if (isset($salaryParts[1])) {
+                    $maxSalary = (int) str_replace(',', '', trim($salaryParts[1]));
+                }
+            }
+
+            if ($data[10] != 'n/a') {
+                $experience = (float) $data[10];
+
+                if ($experience < 1) {
+                    $experienceValue = 'fresher';
+                } elseif ($experience >= 1 && $experience < 3) {
+                    $experienceValue = '1-3';
+                } elseif ($experience >= 3 && $experience < 5) {
+                    $experienceValue = '3-5';
+                } else {
+                    $experienceValue = '5+';
+                }
+            }
+
+            $minExperience = $experienceValue;
+            $contactEmail = $data[16];
+            $contactPhone = $data[17];
+            $qualification = $data[11];
+            $position = $data[6];
+
+            if ($data[14] != 'Not mentioned ') {
+                if (!empty($data[14])) {
+                    if (is_numeric($data[14])) {
+                        $date = Date::excelToDateTimeObject($data[14]);
+                    } else {
+                        $date = Carbon::parse($data[14]);
+                    }
+
+                    $formattedDate = Carbon::instance($date)
+                        ->addDays(30)
+                        ->format('Y-m-d');
+                }
+            }
+
+            if ($schoolName) {
+                JobPost::create([
+                    'school_name' =>  $schoolName,
+                    'city_id' =>  $city->id,
+                    'subject_id' =>  $subject ? $subject->id : NULL,
+                    'grade_id' =>  $grade ? $grade->id : NULL,
+                    'food' =>  0,
+                    'accommodation' =>  0,
+                    'job_type' =>  "Full Time",
+                    'application_deadline' =>  $formattedDate,
+                    'min_salary' =>  $minSalary,
+                    'max_salary' =>  $maxSalary,
+                    'experience_required' =>  $minExperience,
+                    'job_description' =>  $qualification,
+                    'qualification_requirements' =>  $qualification,
+                    'contact_email' =>  $contactEmail,
+                    'contact_phone' =>  $contactPhone,
+                    'status' =>  1,
+                    'position' =>  $position,
+                    'board' => $board
+                ]);
             }
         }
     }
